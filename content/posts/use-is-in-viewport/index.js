f09a7b3622ba091ff6bfb7b07376f7435da7d9ae
Nevoss <nevos12@gmail.com>
2021-01-31 23:11:21 +0200

    fix title

--- content/posts/use-is-in-viewport/index.js
+++ content/posts/use-is-in-viewport/index.js
@@ -1,7 +1,7 @@
 import coverImage from './cover.jpg'
 
 export default {
-  title: 'Is an element in the viewport with React hooks',
+  title: 'Is an HTML element in the viewport? with React hooks',
   excerpt:
     'Create a simple custom hook to determine whether an HTML element is in the browser viewport.',
   date: '2021-01-30 21:00:00',
